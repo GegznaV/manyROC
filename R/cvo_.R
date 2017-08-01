807a26428c1029321fcfa799db8f46efd944e365
GegznaV <GegznaV@gmail.com>
2017-08-01 22:15:24 +0300

    cvo_count_folds moved to a setparate file

--- R/cvo_.R
+++ R/cvo_.R
@@ -32,25 +32,3 @@ cvo_get_seeds <- function(cvo) {
     attr(cvo, "seeds")
 }
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' Count folds in a cvo object
-#'
-#' Function \code{cvo_count_folds} returns total number of folds in a
-#'  \code{cvo} object.
-#' @export
-cvo_count_folds <- function(cvo) {
-    UseMethod("cvo_count_folds")
-}
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' @rdname cvo_count_folds
-#' @export
-cvo_count_folds.cvo_caret <- function(cvo) {
-    length(names(cvo))
-}
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#' @rdname cvo_count_folds
-#' @export
-cvo_count_folds.cvo_mlr <- function(cvo) {
-    cvo$desc$iters
-}
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
