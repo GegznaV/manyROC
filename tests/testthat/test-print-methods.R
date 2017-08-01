c526a8a829605d7982c8478f4645a83394dabeb5
GegznaV <GegznaV@gmail.com>
2017-08-01 19:39:37 +0300

    Bug in print methods test fixed

--- tests/testthat/test-print-methods.R
+++ tests/testthat/test-print-methods.R
@@ -4,13 +4,15 @@ test_that("print methos do not throw error", {
     set.seed(1)
     x_ <- rnorm(10)
     gr_ <- gl(n = 2, k = 5, length = 10)
-    res_list <- roc_analysis(x_, gr_)
 
-    out_1 <- capture_output(print(res_list))
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    res_list_1 <- roc_analysis(x_, gr_)
+    out_1 <- capture_output(print(res_list_1))
     expect_is(out_1, "character")
-
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     data(fluorescence)
-    out_2 <- capture_output(roc_multiroc(fluorescence[ , , 500~502],
-                                         fluorescence$gr))
+    res_list_2 <- roc_multiroc(fluorescence[ , , 500~502], fluorescence$gr)
+    out_2 <- capture_output(print(res_list_2))
     expect_is(out_2, "character")
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 })
