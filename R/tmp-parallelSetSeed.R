085ef5c9741bc6c98a6eeeb11b7f14e105ee49c7
Vilmantas Gegzna <GegznaV@gmail.com>
2018-08-13 21:31:03 +0300

    Documentation bug fixed

--- R/tmp-parallelSetSeed.R
+++ R/tmp-parallelSetSeed.R
@@ -1,12 +1,14 @@
 #' Set seeds for reproducible parallel computing with 'parallelMap' package
 #'
 #' A wrapper for \pkg{parallel}::\code{\link[parallel]{clusterSetRNGStream}}.
-#' @param ... Arguments to be passed to `set.seed()`.
-#' @inheritParams parallel::clusterSetRNGStream
-#' @inheritParams base::set.seed
 #'
+#' @param ... Arguments to be passed to \code{set.seed()}.
+#' @param cl A cluster from either \pkg{parallel}  or package \pkg{snow},
+#'           or (if \code{NULL}) the registered cluster.
+#' @inheritParams parallel::clusterSetRNGStream
+#' @inheritParams set.seed
 #' @export
 parallelSetSeed <- function(iseed = NULL, seed = iseed, cl = NULL, ...) {
-    base::set.seed(iseed, ...)
-    parallel::clusterSetRNGStream(iseed = iseed)
+    set.seed(seed = seed, ...)
+    parallel::clusterSetRNGStream(iseed = iseed, cl = cl)
 }
