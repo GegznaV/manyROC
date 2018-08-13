e4adb33ae25d5a82d34ef410794158ab3985a813
Vilmantas Gegzna <GegznaV@gmail.com>
2018-08-13 19:11:22 +0300

    ParallelSetSeed() updated

--- R/tmp-parallelSetSeed.R
+++ R/tmp-parallelSetSeed.R
@@ -1,10 +1,12 @@
 #' Set seeds for reproducible parallel computing with 'parallelMap' package
 #'
 #' A wrapper for \pkg{parallel}::\code{\link[parallel]{clusterSetRNGStream}}.
-#'
+#' @param ... Arguments to be passed to `set.seed()`.
 #' @inheritParams parallel::clusterSetRNGStream
+#' @inheritParams base::set.seed
 #'
 #' @export
-parallelSetSeed <- function(iseed) {
-    parallel::clusterSetRNGStream(iseed = NULL)
+parallelSetSeed <- function(iseed = NULL, seed = iseed, cl = NULL, ...) {
+    base::set.seed(iseed, ...)
+    parallel::clusterSetRNGStream(iseed = iseed)
 }
